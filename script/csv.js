ff1fea1689a7967196e58959ab956e59e1a6671d
ANA PAULA COSTACURTA <anapaula.costacurta@ifpr.edu.br>
2025-07-11 00:37:30 +0000

    Versão 10.9

--- script/csv.js
+++ script/csv.js
@@ -21,10 +21,10 @@ document.addEventListener("DOMContentLoaded", carregarRanking);
 
       // Converte o CSV em um array de objetos com total calculado
       const ranking = lines.map(line => {
-        const [email, atv1, atv2, atv3, resg, atv4, total] = line.split(',').map(val => val.trim());
-        const total_calculado = (parseInt(atv1) || 0) + (parseInt(atv2) || 0) + (parseInt(atv3) || 0) + (parseInt(atv4) || 0) - (parseInt(resg) || 0);
+        const [email, atv1, atv2, atv3, resg, atv4, quiz1, total] = line.split(',').map(val => val.trim());
+        const total_calculado = (parseInt(atv1) || 0) + (parseInt(atv2) || 0) + (parseInt(atv3) || 0) + (parseInt(atv4) || 0) + (parseInt(quiz1) || 0) - (parseInt(resg) || 0);
         console.log(total)
-        return { email, atv1, atv2, atv3, atv4, resg, total_calculado, total };
+        return { email, atv1, atv2, atv3, atv4, resg, quiz1, total_calculado, total };
       });
 
       // Ordena do maior para o menor total
