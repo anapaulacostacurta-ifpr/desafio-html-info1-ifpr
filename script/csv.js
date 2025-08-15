c4bea1ce583373f41bd1e858f71693531dc7ff88
ANA PAULA COSTACURTA <anapaula.costacurta@ifpr.edu.br>
2025-08-15 17:46:59 +0000

    Versao 11.5

--- script/csv.js
+++ script/csv.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", carregarRanking);
 
       // Converte o CSV em um array de objetos com total calculado
       const ranking = lines.map(line => {
-        const [email, atv1, atv2, atv3, resg, atv4, quiz1, total] = line.split(',').map(val => val.trim());
+        const [email, atv1, atv2, atv3, resg, atv4, quiz1, quiz2, total] = line.split(',').map(val => val.trim());
         const total_calculado = (parseInt(atv1) || 0) + (parseInt(atv2) || 0) + (parseInt(atv3) || 0) + (parseInt(atv4) || 0) + (parseInt(quiz1) || 0) + (parseInt(quiz2) || 0)- (parseInt(resg) || 0);
         console.log(total)
         return { email, atv1, atv2, atv3, atv4, resg, quiz1, quiz2,total_calculado, total };
